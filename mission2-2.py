553b2e4d8b85b8467ecfcc26e0743e01b9cd58a3
DESKTOP-6F3759O\hyemin <gpals5987@gmail.com>
2024-09-16 04:02:00 +0900

    [feat] 미션2-2

--- mission2-2.py
+++ mission2-2.py
@@ -1,16 +1,30 @@
+#런2_B
 from jikko.jikko import *
 jk = Pyjikko()
 
-PORT = 'COM9'
-LED = 5
-BUZZER = 6
+PORT = 'COM7'
+SERVO = 8
+TRIG = 13
+ECHO = 12
 
 jk.serial_connect(PORT)
 jk.start()
+
+jk.lcd_set(0x27, 16, 2)
 
 while True:
     distance = jk.sonic_read(TRIG, ECHO)
     jk.lcd_display(0,0,str(distance))
     time.sleep(1)
-    
-    if distance < 20
+
+    if distance < 20:
+        jk.servo_degree(SERVO, 90)
+        jk.lcd_display(0,1, "open")
+        time.sleep(2)
+
+    if distance > 20:
+        jk.servo_degree(SERVO, 0)
+        jk.lcd_display(0,1, "close")
+        time.sleep(1)
+
+    jk.lcd_clear()
